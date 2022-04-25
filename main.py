686fc85bbb9743f3b38b5ade77854bac3bb2c1fe
Aracely1607 <aracely.perez97@gmail.com>
2022-04-24 21:23:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,18 @@ interfaz.relleno()
 
 #Aumenta a cada caballo un numero aleatorio de pasos entre 0 y 6
 while Caballo1<150 and Caballo2<150 and Caballo3<150 and Caballo4<150:
+    
     Caballo1 = Caballo1 + random.randint(0,6)
     Caballo2 = Caballo2 + random.randint(0,6)
     Caballo3 = Caballo3 + random.randint(0,6)
     Caballo4 = Caballo4 + random.randint(0,6)
 
     #Llama a la funcion caballo_* (diseño del caballo)
+    os.System("clr")
     interfaz.caballo_1(Caballo1)
     interfaz.caballo_2(Caballo2)
     interfaz.caballo_3(Caballo3)
     interfaz.caballo_4(Caballo4)
-    #os.System("clr")
     print("====================================================================================================================================================================")
     time.sleep(0.05)
 
